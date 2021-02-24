27658e314c69f60dc2b0035f156be8d35ba18780
okminz <okminz@gmail.com>
2021-02-24 04:52:35 -0800

    Create Login + Callback

--- app.py
+++ app.py
@@ -1,43 +1,57 @@
 import flask
-import requests_oauthlib
+from authlib.integrations import requests_client
 import requests
 import pickle
+import sys
+import os
+
+from stackapi import StackAPI
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
 
 # Use pickle to load in model
 # with open(f'model/first_model.pkl', 'rb') as f:
 #     model = pickle.load(f)
 
-AUTH_BASE_URL = "https://stackexchange.com/oauth/dialog?client_id=19673&scope=&redirect_uri=http://localhost:5000/"
+AUTH_BASE_URL = "https://stackexchange.com/oauth"
+TOKEN_URL = "https://stackoverflow.com/oauth/access_token/json"
 CLIENT_ID = "19673"
+CLIENT_SECRET =  ""
+SECRET_KEY = ""
 
 app = flask.Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
+    flask.render_template('main.html')
     return """
-        <a href="/login">Login with SuperUser</a>
+        <a href="/login">Login with StackExchange: SuperUser</a>
     """
-
-    # if flask.request.method == 'GET':
-    #     return(flask.render_template('main.html'))
-    
-    # if flask.request.method == 'POST':
-    #     returnVal = requests.get('').content
-    #     print(returnVal)
-        
-
-    # return(flask.render_template('main.html'))
 
 @app.route('/login')
 def login():
-    superuser = requests_oauthlib.OAuth2Session(CLIENT_ID, redirect_uri="http://localhost:5000/callback")
-    auth_url, _ = superuser.authorization_url(AUTH_BASE_URL)
+    superuser = requests_client.OAuth2Session(CLIENT_ID, redirect_uri="http://localhost:5000/callback")
+    auth_url, _ = superuser.create_authorization_url(AUTH_BASE_URL)
 
     return flask.redirect(auth_url)
 
 @app.route('/callback')
 def callback():
-    superuser = requests_oauthlib.OAuth2Session(CLIENT_ID)
+    
+    superuser = requests_client.OAuth2Session(CLIENT_ID)
+    token = superuser.fetch_token(
+    	url=TOKEN_URL, client_secret=CLIENT_SECRET, \
+        authorization_response=flask.request.url, \
+        redirect_uri="http://localhost:5000/callback"
+	)
+
+    SITE = StackAPI('superuser', key=SECRET_KEY)
+    me = SITE.fetch('me', access_token=token['access_token'])
+
+    # Keep user_id, profile_image, display_name
+
+    return flask.json.jsonify(me)
 
 @app.route('/', methods=['GET', 'POST'])
 def get_data():
