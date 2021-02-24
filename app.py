9da9f397dd7cf19005d0f44b53c0f10936f27041
okminz <okminz@gmail.com>
2021-02-24 02:44:08 -0800

    updates

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
 import flask
+# import requests_oauthlib
+import requests
+import pickle
+
+# Use pickle to load in model
+# with open(f'model/first_model.pkl', 'rb') as f:
+#     model = pickle.load(f)
+
+AUTH_BASE_URL = "https://stackexchange.com/oauth/dialog?client_id=19673&scope=&redirect_uri=http://localhost:5000/"
+
 app = flask.Flask(__name__, template_folder='templates')
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    return(flask.render_template('main.html'))
+    return """
+        <a href="/login">Login with SuperUser</a>
+    """
+
+    # if flask.request.method == 'GET':
+    #     return(flask.render_template('main.html'))
+    
+    # if flask.request.method == 'POST':
+    #     returnVal = requests.get('').content
+    #     print(returnVal)
+        
+
+    # return(flask.render_template('main.html'))
+
+@app.route('/', methods=['GET', 'POST'])
+def get_data():
+    returnVal = requests.get('https://stackexchange.com/oauth/dialog?client_id=19673&scope=&redirect_uri=http://localhost:5000/').content
+    print(returnVal)
+    return returnVal
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
