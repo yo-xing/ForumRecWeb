24e6f562abe9ecfa68bfaae8569277d9a58cbced
okminz <okminz@gmail.com>
2021-02-24 05:42:41 -0800

    update requirements: stackapi; app: added URL

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import flask
 from authlib.integrations import requests_client
-import requests
 import pickle
 import sys
 import os
@@ -31,20 +30,21 @@ def main():
 
 @app.route('/login')
 def login():
-    superuser = requests_client.OAuth2Session(CLIENT_ID, redirect_uri="http://localhost:5000/callback")
+    superuser = requests_client.OAuth2Session(CLIENT_ID, redirect_uri="http://jackzlin.com/callback")
+    #superuser = requests_client.OAuth2Session(CLIENT_ID, redirect_uri="http://localhost:5000/callback")
     auth_url, _ = superuser.create_authorization_url(AUTH_BASE_URL)
 
     return flask.redirect(auth_url)
 
 @app.route('/callback')
 def callback():
-    
+
     superuser = requests_client.OAuth2Session(CLIENT_ID)
     token = superuser.fetch_token(
     	url=TOKEN_URL, client_secret=CLIENT_SECRET, \
         authorization_response=flask.request.url, \
-        redirect_uri="http://localhost:5000/callback"
-	)
+        redirect_uri="http://jackzlin.com/callback" )
+        #redirect_uri="http://localhost:5000/callback" )
 
     SITE = StackAPI('superuser', key=SECRET_KEY)
     me = SITE.fetch('me', access_token=token['access_token'])
@@ -55,7 +55,8 @@ def callback():
 
 @app.route('/', methods=['GET', 'POST'])
 def get_data():
-    returnVal = requests.get('https://stackexchange.com/oauth/dialog?client_id=19673&scope=&redirect_uri=http://localhost:5000/').content
+    returnVal = requests.get('https://stackexchange.com/oauth/dialog?client_id=19673&scope=&redirect_uri=http://jackzlin.com/').content
+    #returnVal = requests.get('https://stackexchange.com/oauth/dialog?client_id=19673&scope=&redirect_uri=http://localhost:5000/').content
     print(returnVal)
     return returnVal
 
