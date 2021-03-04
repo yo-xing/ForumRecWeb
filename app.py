b2b5d46ce9d5f13da4373bfff60a741288776090
Jasraj Johl <jasrajjohl@gmail.com>
2021-03-04 04:14:35 -0800

    Switched Redirect URI

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ def main():
 
 @app.route('/login')
 def login():
-    # superuser = requests_client.OAuth2Session(CLIENT_ID, redirect_uri="http://jackzlin.com/callback")
-    superuser = requests_client.OAuth2Session(CLIENT_ID, redirect_uri="http://localhost:5000/callback")
+    superuser = requests_client.OAuth2Session(CLIENT_ID, redirect_uri="http://jackzlin.com/callback")
+    # superuser = requests_client.OAuth2Session(CLIENT_ID, redirect_uri="http://localhost:5000/callback")
     auth_url, _ = superuser.create_authorization_url(AUTH_BASE_URL)
 
     return flask.redirect(auth_url)
@@ -77,8 +77,8 @@ def callback():
     token = superuser.fetch_token(
     	url=TOKEN_URL, client_secret=CLIENT_SECRET, \
         authorization_response=flask.request.url, \
-        # redirect_uri="http://jackzlin.com/callback" )
-        redirect_uri="http://localhost:5000/callback" )
+        redirect_uri="http://jackzlin.com/callback" )
+        # redirect_uri="http://localhost:5000/callback" )
 
     SITE = StackAPI('superuser', key=SECRET_KEY)
     me = SITE.fetch('me', access_token=token['access_token'])
